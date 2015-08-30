1f9838b21b980ee5742694c761dbcfc08f17aa45
martaniatd <martaniadewi@yahoo.com>
2015-08-30 18:21:43 +0700

    Update exercise2.cpp

---
+++ exercise2.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h> 
+#include <conio.h>
+int main() { 
+clrscr(); 
+int a, b,hasil;
+hasil=a^b; 
+cout << " Input Number = " ;
+cin>>a;
+cout << " Input Power = ";
+cin>>b;
+cout << " d = " << hasil << '\n'; 
+getch(); 
+}
+
