884c3a6490ac222b2024135ac73ec57367131ded
martaniatd <martaniadewi@yahoo.com>
2015-08-30 21:39:06 +0700

    Martania exercise 3.cpp

---
+++ exercise3.cpp
@@ -0,0 +1,20 @@
+main ()
+{
+int a,b,c;
+char hasil;
+clrscr();
+cout<<" Side 1 : ";
+cin>>a;
+cout<<" Side 2 : ";
+cin>>b;
+cout<<" Side 3 : ";
+cout>>c;
+cout<<" Output : "<<hasil;
+if(a==c)
+cout<<hasil="isosceles"
+else if (a==b)
+cout<<hasil="Equilateral"
+else
+  cout<<hasil="scalene"
+  
+getch();
